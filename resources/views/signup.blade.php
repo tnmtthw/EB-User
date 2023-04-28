20854bd21d54ea99b7a368c04b50811206f63bfb
aliriocerafica <ceraficaalejandrojr@gmail.com>
2023-04-28 23:36:21 +0800

    edited services and buttons hover in user homepage

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
@@ -27,6 +26,7 @@
 
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
+
 
   <!-- =======================================================
   * Template Name: Arsha

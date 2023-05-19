ffe2307520289baee2052b6f8f5cfe6651a54068
aliriocerafica <ceraficaalejandrojr@gmail.com>
2023-05-19 22:49:20 +0800

    Request Page Modified style.css updated

--- resources/views/request.blade.php
+++ resources/views/request.blade.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<htm llang="en">
 
 <head>
-  <meta charset="utf-8">
+  <m ea charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>E-Barangay</title>
+  <t ile>E-Barangay</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
-
+ 
   <!-- Favicons -->
   <link href="assets/img/logo3.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <l ik href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <l ik href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <l ik href="asses/vendor/aos/aos.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
@@ -287,14 +287,24 @@ c;
     <div class="container d-flex align-items-center">
 
     <h1 class="logo me-auto" style="align-content: left;"><a href="index.php">EBarangay</a></h1>
-    
-
-    @include('include.navbar-inside')
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a  href="{{ url('/') }}">Home</a></li>
+                <li><a href="">Dashboard</a></li>
+        
+          <!-- <li><a  href="{{ route('logout')}}">Logout</a></li> -->
+          <div class="container1">
+ 		<a style="color: white"><span id='clock'></span></a>
+</div>
+          
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
 
     </div>
   </header><!-- End Header -->
 
- <body  >
+ <body>
 
   <!-- ======= Services Section ======= -->
   <main id="main">
@@ -396,11 +406,13 @@ c;
 
       </div>
 
-    </section><!-- End Services Section -->
+    </section>
+    <!-- End Services Section -->
 
     
   </main>
-    </body><!-- End Services Section -->
+    </body>
+    <!-- End Services Section -->
 
  
 
@@ -466,3 +478,30 @@ c;
 </body>
 
 </html>
+<script>
+
+	var dt = new Date();
+var hours = dt.getHours();
+var minutes = dt.getMinutes();
+var ampm = hours >= 12 ? 'pm' : 'am';
+hours = hours % 12;
+hours = hours ? hours : 12; // the hour '0' should be '12'
+var timeString = hours + ':' + minutes + '' + ampm;
+document.getElementById('clock').innerHTML = dt.toDateString() + ' ' + timeString;
+
+function clock() {
+    var dt = new Date();
+    var hours = dt.getHours();
+    var minutes = dt.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    var timeString = hours + ':' + minutes + '' + ampm;
+    clockElement.textContent = dt.toDateString() + ' ' + timeString;
+}
+
+setInterval(clock, 1000);
+
+
+
+  </script>
